ce4d2482b72436bda41e9fee40f85e11fac8732f
Paweł Kałdus <xdk78888@gmail.com>
2022-12-17 14:35:44 +0100

    Add sending new shorten link form in homepage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Link;
+use App\Repository\LinkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 #[Route('/')]
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request, LinkRepository $linkRepository): Response
     {
-        return $this->render('index/index.html.twig');
+        $link = new Link();
+
+        $form = $this->createFormBuilder($link)
+            ->add('fullLink', TextType::class, ['required' => true])
+            ->add('link', TextType::class, ['required' => false])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $link->setCreatedAt(new \DateTime());
+            $link->setUpdatedAt(new \DateTime());
+            $link->setUsesCount(0);
+            $link->setIsActive(true);
+            if ($link->getLink() == null) {
+                $link->setLink(hash('crc32', $link->getFullLink()));
+            }
+
+            $linkRepository->save($link, true);
+
+            return $this->redirectToRoute('app_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render(
+            'index/index.html.twig',
+            [
+                'form' => $form->createView(),
+                'links' => $linkRepository->findAll(),
+            ]
+        );
     }
 }
